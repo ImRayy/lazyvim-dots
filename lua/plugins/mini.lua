e670e34f155d6d4f71a8ca190d97e7e877bc59eb
ImRayy <souradeep_roy@pm.me>
2026-08-25 12:46:24 +0530

    feat: customize mini.surround keybindings

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -25,4 +25,22 @@ return {
       },
     },
   },
+
+  -- Fast and feature-rich surround actions
+  {
+    "nvim-mini/mini.surround",
+    events = { "BufWritePost", "BufReadPost", "InsertLeave" },
+    version = "*",
+    opts = {
+      mappings = {
+        add = "gsa", -- Add surrounding in Normal and Visual modes
+        delete = "gsd", -- Delete surrounding
+        find = "gsf", -- Find surrounding (to the right)
+        find_left = "gsF", -- Find surrounding (to the left)
+        highlight = "gsh", -- Highlight surrounding
+        replace = "gsr", -- Replace surrounding
+        update_n_lines = "gsn", -- Update `n_lines`
+      },
+    },
+  },
 }
